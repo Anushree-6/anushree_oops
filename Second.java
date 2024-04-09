84e96280d5e4c094b736baf33c3acecc6bc5c3a4
anushree_oops <anushreesawant362@gmail.com>
2024-04-09 11:51:34 +0530

    Successfully Executed

---
+++ Second.java
@@ -0,0 +1,12 @@
+package anushree;
+
+import learnjava.First;
+
+public class Second
+{
+    public static void main(String[] args)
+    {
+        First firstObject = new First();
+        firstObject.helloWorld();
+    }
+}
